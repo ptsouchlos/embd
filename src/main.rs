78b886cf614e01654949373adb4d5f80a3c0fc6b
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-04-24 15:38:37 -0400

    chore: reorg main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use clap::Parser;
 use crate::commands::add::AddArgs;
 mod commands;
 mod config;
+mod filesystem;
+mod git;
 mod paths;
 
 #[derive(Parser, Debug)]
@@ -19,17 +21,18 @@ struct Options {
     command: Command,
 }
 
-fn main() {
-    if let Err(e) = run() {
-        eprintln!("error: {e:#}");
-        std::process::exit(1);
-    }
-}
-
+/// Main function that runs the CLI. It parses the arguments and dispatches to the appropriate command handler.
 fn run() -> Result<()> {
     let opts = Options::parse();
     match opts.command {
         Command::Add(add_args) => commands::add::execute(add_args)?,
     }
     Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("error: {e:#}");
+        std::process::exit(1);
+    }
 }
