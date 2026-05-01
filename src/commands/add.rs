2592ea408370b6f7abcd04af686645a3fd7f47ea
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-04-30 23:00:01 -0400

    fix: don't require name for add command

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,15 +1,15 @@
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use anyhow::{Context, Result, bail};
 use tempfile::tempdir;
+use url::Url;
 
 use crate::config::{Config, EmbdEntry};
 use crate::{filesystem, git, paths};
 
 #[derive(clap::Args, Debug)]
 pub(crate) struct AddArgs {
-    #[clap(short, long, help = "Name of the entry")]
-    name: String,
     #[clap(short, long, help = "Link to the repository")]
     link: String,
     #[clap(short, long, help = "Path to pull the files to")]
@@ -39,13 +39,6 @@ pub(crate) fn execute(args: AddArgs) -> Result<()> {
         }
     };
 
-    if config.contains(&args.name) {
-        bail!(
-            "an embed named '{}' already exists in the config",
-            args.name
-        );
-    }
-
     if args.folder.is_dir() && !args.allow_untracked {
         let mut entries = std::fs::read_dir(&args.folder)
             .with_context(|| format!("failed to read directory {}", args.folder.display()))?;
@@ -62,8 +55,16 @@ pub(crate) fn execute(args: AddArgs) -> Result<()> {
     let commit_hash = git::cli::commit_hash_of(tmp.path())?;
     filesystem::copy_dir(tmp.path(), &args.folder)?;
 
+    // Get the name from the repo link
+    let url = Url::from_str(&args.link)?;
+    let segments = url.path_segments().context("Could not split URL")?;
+    let repo_name = segments
+        .last()
+        .context("Could not get repo name from {args.link}")?
+        .replace(".git", "");
+
     config.insert(
-        args.name,
+        repo_name.to_string(),
         EmbdEntry {
             remote: args.link,
             commit_hash,
